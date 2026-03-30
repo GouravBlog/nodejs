f5ecbe586468da18c3ad6d54cf0e3eacb61cc123
Gourav <gouravvasale@gmail.com>
2026-03-30 13:39:03 +0530

    Clean commit without secrets

---
+++ OAuth/app/google.js
@@ -0,0 +1,22 @@
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require("passport");
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+},
+    function (accessToken, refreshToken, profile, cb) {
+        // console.log('profile', profile)
+        return cb(null, profile);
+    }
+));
+
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
